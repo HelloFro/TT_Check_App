554fb963a2389d83c0dbc4224131403f6f9d5d28
Fro Warner <froennkastle@gmail.com>
2020-08-12 13:00:48 -0700

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:tt_check/screens/home.dart';
+
 class App extends StatelessWidget {
   const App({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      themeMode: ThemeMode.system,
+      home: Home(),
     );
   }
 }
