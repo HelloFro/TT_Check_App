3ad356e76e165d130b04d8f61f936c4ec5b710e1
Fro Warner <froennkastle@gmail.com>
2020-08-12 13:05:05 -0700

    Update piece_bench.dart

--- lib/game/piece_bench.dart
+++ lib/game/piece_bench.dart
@@ -11,7 +11,7 @@ class PieceBench extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
       child: Row(
         children: <Widget>[
           Piece(name: "Pawn", piece: WhitePawn(size: kPieceSize)),
