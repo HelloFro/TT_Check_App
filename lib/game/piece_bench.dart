639112aa9ef9708b97e61224bb4cb96d67496099
Fro Warner <froennkastle@gmail.com>
2020-08-12 13:31:56 -0700

    Update piece_bench.dart

--- lib/game/piece_bench.dart
+++ lib/game/piece_bench.dart
@@ -7,17 +7,28 @@ import 'package:tt_check/constants.dart';
 import 'package:tt_check/game/piece.dart';
 
 class PieceBench extends StatelessWidget {
-  const PieceBench({Key key}) : super(key: key);
+  const PieceBench({Key key, this.player}) : super(key: key);
+
+  final Player player;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
+    return Container(
+      child: player == Player.white ? Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Piece(name: "Pawn", piece: WhitePawn(size: kPieceSize)),
-          Piece(name: "Rook", piece: WhiteRook(size: kPieceSize)),
-          Piece(name: "Bishop", piece: WhiteBishop(size: kPieceSize)),
-          Piece(name: "King", piece: WhiteKing(size: kPieceSize)),
+          Piece(name: "Pawn", piece: WhitePawn(size: kPieceSize), player: player),
+          Piece(name: "Rook", piece: WhiteRook(size: kPieceSize), player: player),
+          Piece(name: "Bishop", piece: WhiteBishop(size: kPieceSize), player: player),
+          Piece(name: "King", piece: WhiteKing(size: kPieceSize), player: player),
+        ],
+      ) : Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Piece(name: "Pawn", piece: BlackPawn(size: kPieceSize), player: player),
+          Piece(name: "Rook", piece: BlackRook(size: kPieceSize), player: player),
+          Piece(name: "Bishop", piece: BlackBishop(size: kPieceSize), player: player),
+          Piece(name: "King", piece: BlackKing(size: kPieceSize), player: player),
         ],
       ),
     );
