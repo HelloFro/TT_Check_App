55e1299266aece5bc6df9c13a9447695641efe2e
Fro Warner <froennkastle@gmail.com>
2020-08-12 13:01:33 -0700

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tt_check/game/piece_bench.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
@@ -6,6 +7,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: PieceBench(),
     );
   }
 }
