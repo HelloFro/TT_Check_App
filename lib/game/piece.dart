eac4186418840279a29ef343987683daf807566e
Fro Warner <froennkastle@gmail.com>
2020-08-12 22:45:52 -0700

    Update piece.dart

--- lib/game/piece.dart
+++ lib/game/piece.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:tt_check/game/game_state.dart';
+import 'package:tt_check/models/piece_data.dart';
 
 import '../constants.dart';
 
 class Piece extends StatelessWidget {
-  const Piece({Key key, this.piece, this.name, this.player}) : super(key: key);
+  const Piece({Key key, this.pieceData}) : super(key: key);
 
-  final String name;
-  final Widget piece;
-  final Player player;
+  final PieceData pieceData;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Draggable(
-        child: piece, 
-        feedback: piece,
-        childWhenDragging: player == Player.white ? Placeholder(color: Colors.white): Placeholder(color: Colors.black),
+        data: pieceData,
+        child: pieceData.piece, 
+        feedback: pieceData.piece,
+        childWhenDragging: Opacity(child: pieceData.piece, opacity: .5),
         // TODO: implement Turn Logic
-        onDragCompleted: null,
+        onDragCompleted: () { 
+          turnStateProvider.read(context).state = turnStateProvider.read(context).state == Player.black ? Player.white: Player.black;
+          turnCounterStateProvider.read(context).state++;
+          print("It is now " + turnStateProvider.read(context).state.toString() + "'s turn.");
+        },
+        onDragStarted: () => print("onDragStarted Player Turn: " + turnStateProvider.read(context).state.toString()),
       ),
       width: kPieceSize, 
       height: kPieceSize,
