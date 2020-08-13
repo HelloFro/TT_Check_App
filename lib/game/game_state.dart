3b04dc37dd5e36df814fcd91697cd8f4f6cc8d6c
Fro Warner <froennkastle@gmail.com>
2020-08-12 23:41:37 -0700

    Revert "Revert "Update game_state.dart""
    This reverts commit 94197f768c21fe191ba93828b0919e1b8e5e2e19.

--- lib/game/game_state.dart
+++ lib/game/game_state.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:tt_check/models/game.dart';
+
 import '../constants.dart';
 
+final gameProvider = Provider((ref) => Game());
 
 final startingPlayerProvider = StateProvider((ref) => Player.black);
 
@@ -10,3 +15,11 @@ final turnStateProvider = StateProvider((ref) => Player.black);
 final restartStateProvider = StateProvider((ref) => false);
 
 final turnCounterStateProvider = StateProvider((ref) => 0);
+
+final sharedPreferenceInstanceProvider = Provider((ref) async => await SharedPreferences.getInstance());
+
+Future<void> setStartingPlayer(Player player) async => 
+  await SharedPreferences.getInstance().then((pref) => pref.setInt('player', player == Player.black ? 0 : 1));
+
+Future<Player> getStartingPlayerFromSP() async => 
+  await SharedPreferences.getInstance().then((pref) => pref.containsKey('player') ? (pref.getInt('player') == 0 ? Player.black : Player.white) : Player.black);
